c461925e106a155c9cb05198f7c133601f6735bc
Francisco Medina <tuemail@email.com>
2026-03-14 16:53:37 +0100

    Cambiado nombre de Cuadrado a Cuadrado2 :p

--- Cuadrado.java
+++ Cuadrado.java
@@ -2,8 +2,8 @@ package figuras;
 
 import java.awt.Color;
 
-public class Cuadrado extends Rectángulo {
-    public Cuadrado(double x, double y, Color color, double lado) {
+public class Cuadrado2 extends Rectángulo {
+    public Cuadrado2(double x, double y, Color color, double lado) {
         super(x, y, color, lado, lado);
     }
 }
